569f8edeee49239048872da7e348ca324954e147
Marcel Rösler <roesler1@gmx.de>
2017-04-09 12:47:36 +0200

    added authntificationdata from file

--- src/app/actions/getJson.js
+++ src/app/actions/getJson.js
@@ -1,15 +1,23 @@
 import base64 from 'base-64';
+import userdata from '../constants/baseAuth';
+
 export default function getJson(){
     return (dispatch) => {
-        fetch('http://192.168.0.102:3000/all')
-            .then((response) => {
-                return response.json()
-            })
-            .then((responseJson) => {
-                dispatch({type: "GOT_JSON", json: responseJson})
-            })
-            .catch((error) => {
-                console.error(error);
-            });
+        fetch('http://192.168.0.102:3000/all',{
+            method: "get",
+            headers:{
+                "Authorization": "Basic "+base64.encode(userdata.username+":"+userdata.password),
+                "Content-Type": "application/x-www-form-urlencoded"
+            }
+        })
+        .then((response) => {
+            return response.json()
+        })
+        .then((responseJson) => {
+            dispatch({type: "GOT_JSON", json: responseJson})
+        })
+        .catch((error) => {
+            console.error(error);
+        });
     }
 }
