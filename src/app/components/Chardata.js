179f906fbdb920b13324fa1e6c84d3adc256c04f
Marcel Rösler <roesler1@gmx.de>
2017-04-09 14:10:32 +0200

    more values

--- src/app/components/Chardata.js
+++ src/app/components/Chardata.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, ViewStyle, Picker, StatusBar} from 'react-native';
+import { StyleSheet, Text, View, ScrollView} from 'react-native';
 import { connect } from 'react-redux';
 
 class App extends Component {
@@ -8,25 +8,71 @@ class App extends Component {
     }
 
     render() {
-        return (
-            <View style={styles.charContainer}>
-                <Text>{this.props.char["$"].name}</Text>
-                {Object.keys(this.props.char["basis"][0]).map(key=>{
-                        if(key === "geschlecht"){
-                            return <Text key={Math.random()}>{"Geschlecht: "+this.props.char["basis"][0][key][0]["$"].name}</Text>
-                        }else if(key === "rasse"){
-                            return <Text key={Math.random()}>{"Rasse: "+this.props.char["basis"][0][key][0]["$"].string}</Text>
+        return <View style={styles.charContainer}>
+                <ScrollView style={styles.scrollview}
+                            automaticallyAdjustContentInsets={false}
+                            horizontal={true}>
+                    <View>
+                        {/**Char Name*/}
+                        <Text>{this.props.char["$"].name}</Text>
+                        {/**Char Basic*/}
+                        {Object.keys(this.props.char["basis"][0]).map(key=>{
+                                if(key === "geschlecht"){
+                                    return <Text key={Math.random()}>{"Geschlecht: "+this.props.char["basis"][0][key][0]["$"].name}</Text>
+                                }else if(key === "rasse"){
+                                    return <Text key={Math.random()}>{"Rasse: "+this.props.char["basis"][0][key][0]["$"].string}</Text>
+                                }
+                                return <Text key={Math.random()}>{key}</Text>
+                            })
                         }
-                        return <Text key={Math.random()}>{key}</Text>
-                    })
-                }
-                {this.props.char["eigenschaften"][0]["eigenschaft"].map(propertie=>{
-                    return <Text key={Math.random()}>{propertie["$"].name+": "+propertie["$"].value}</Text>
-                })
-
-                }
+                        <View style={{height: 40}}>
+                        </View>
+                        {/**Char Properties*/}
+                        {this.props.char["eigenschaften"][0]["eigenschaft"].map(propertie=>{
+                            return <Text key={Math.random()}>{propertie["$"].name+": "+propertie["$"].value}</Text>
+                        })}
+                    </View>
+                    <View>
+                        {/**Char Vor- und Nachteile*/}
+                        {this.props.char["vt"][0]["vorteil"].map(advantage=>{
+                            let value = ""
+                            if("value" in advantage["$"]){value =": "+advantage["$"]["value"]}
+                            return <Text key={Math.random()}>{advantage["$"].name+value}</Text>
+                        })}
+                        <View style={{height: 40}}>
+                        </View>
+                        {/**Char Sonderfertigkeiten*/}
+                        {this.props.char["sf"][0]["sonderfertigkeit"].map(specialCraft=>{
+                            let value = ""
+                            if(Object.keys(specialCraft).length > 1){
+                                value = ": "
+                                Object.keys(specialCraft).map(key =>{
+                                    if(key !== "$"){
+                                        value += specialCraft[key].map(elem =>{
+                                            if(elem["$"].name !== undefined){
+                                                return elem["$"].name
+                                            }else{
+                                                console.log(elem)
+                                            }
+                                        }).join(", ")
+                                    }
+                                })
+                            }
+                            return <Text key={Math.random()}>{specialCraft["$"].name+value}</Text>
+                        })}
+                    </View>
+                    <View>
+                        <ScrollView>
+                            {/**Char Vor- und Nachteile*/}
+                            {this.props.char["talentliste"][0]["talent"].map(ability=>{
+                                return <Text key={Math.random()}>
+                                    {ability["$"].name+": "+ability["$"]["value"]+ability["$"]["probe"]}
+                                </Text>
+                            })}
+                        </ScrollView>
+                    </View>
+                </ScrollView>
             </View>
-        );
     }
 }
 
@@ -34,9 +80,12 @@ const styles = StyleSheet.create({
     charContainer: {
         flex: 1,
         backgroundColor: '#fff',
-        alignItems: 'flex-start',
-        justifyContent: 'flex-start',
+        justifyContent: "flex-start",
+        alignSelf:"stretch",
     },
+    scrollview:{
+        flex:1,
+    }
 });
 
 export default connect((state) => ({
