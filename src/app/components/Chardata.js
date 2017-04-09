f6dd021e32d413e805ae9d8df90de482acdfc285
Marcel Rösler <roesler1@gmx.de>
2017-04-09 14:24:31 +0200

    added sf for elementary master

--- src/app/components/Chardata.js
+++ src/app/components/Chardata.js
@@ -33,33 +33,38 @@ class App extends Component {
                         })}
                     </View>
                     <View>
-                        {/**Char Vor- und Nachteile*/}
-                        {this.props.char["vt"][0]["vorteil"].map(advantage=>{
-                            let value = ""
-                            if("value" in advantage["$"]){value =": "+advantage["$"]["value"]}
-                            return <Text key={Math.random()}>{advantage["$"].name+value}</Text>
-                        })}
-                        <View style={{height: 40}}>
-                        </View>
-                        {/**Char Sonderfertigkeiten*/}
-                        {this.props.char["sf"][0]["sonderfertigkeit"].map(specialCraft=>{
-                            let value = ""
-                            if(Object.keys(specialCraft).length > 1){
-                                value = ": "
-                                Object.keys(specialCraft).map(key =>{
-                                    if(key !== "$"){
-                                        value += specialCraft[key].map(elem =>{
-                                            if(elem["$"].name !== undefined){
-                                                return elem["$"].name
-                                            }else{
-                                                console.log(elem)
-                                            }
-                                        }).join(", ")
-                                    }
-                                })
-                            }
-                            return <Text key={Math.random()}>{specialCraft["$"].name+value}</Text>
-                        })}
+                        <ScrollView>
+                            {/**Char Vor- und Nachteile*/}
+                            {this.props.char["vt"][0]["vorteil"].map(advantage=>{
+                                let value = ""
+                                if("value" in advantage["$"]){value =": "+advantage["$"]["value"]}
+                                return <Text key={Math.random()}>{advantage["$"].name+value}</Text>
+                            })}
+                            <View style={{height: 40}}>
+                            </View>
+                            {/**Char Sonderfertigkeiten*/}
+                            {this.props.char["sf"][0]["sonderfertigkeit"].map(specialCraft=>{
+                                let value = ""
+                                if(Object.keys(specialCraft).length > 1){
+                                    value = ": "
+                                    Object.keys(specialCraft).map(key =>{
+                                        if(key !== "$"){
+                                            value += specialCraft[key].map(elem =>{
+                                                if("wahl" in elem){
+                                                    return elem["wahl"][0]["$"]["value"]+" "+
+                                                        elem["wahl"][1]["$"]["value"]+"("+
+                                                        elem["wahl"][2]["$"]["value"]+")"
+                                                }else{
+                                                    return elem["$"].name
+                                                }
+                                            }).join(", ")
+                                        }
+                                    })
+                                }
+                                console.log(value)
+                                return <Text key={Math.random()}>{specialCraft["$"].name+value}</Text>
+                            })}
+                        </ScrollView>
                     </View>
                     <View>
                         <ScrollView>
